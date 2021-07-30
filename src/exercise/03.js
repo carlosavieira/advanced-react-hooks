467fd74e6c4a592f9793546ec0922c4c2a61d2f9
Carlos Vieira <carlosadrianovieira@gmail.com>
2021-07-30 09:10:11 -0300

    exercise 03 extra 1

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -13,13 +13,21 @@ const CountProvider = (props) => {
   return <CountContext.Provider value={value} {...props} />
 }
 
+const useCount = () => {
+  const context = React.useContext(CountContext)
+  if (!context) {
+    throw new Error(`useCount needs to be in a CountContext`)
+  }
+  return context
+}
+
 function CountDisplay() {
-  const [count] = React.useContext(CountContext)
+  const [count] = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  const [, setCount] = React.useContext(CountContext)
+  const [, setCount] = useCount()
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
