f559ab89734d0ed2dc84d7ad15a19ffc84eb5a29
Carlos Vieira <carlosadrianovieira@gmail.com>
2021-08-08 01:06:55 -0300

    exercise 06

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -7,6 +7,7 @@ function useMedia(query, initialState = false) {
   const [state, setState] = React.useState(initialState)
   // 🐨 call React.useDebugValue here.
   // 💰 here's the formatted label I use: `\`${query}\` => ${state}`
+  React.useDebugValue(`\`${query}\` => ${state}`)
 
   React.useEffect(() => {
     let mounted = true
@@ -36,7 +37,7 @@ function Box() {
   const isSmall = useMedia('(max-width: 699px)')
   const color = isBig ? 'green' : isMedium ? 'yellow' : isSmall ? 'red' : null
 
-  return <div style={{width: 200, height: 200, backgroundColor: color}} />
+  return <div style={{ width: 200, height: 200, backgroundColor: color }} />
 }
 
 function App() {
